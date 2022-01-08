dbee6f79f696e55e51392be9aeaddb30e87377b5
Russell Helmstedter <rhelmstedter@gmail.com>
2022-01-08 11:41:55 -0800

    submission Bite 15 @ codechalleng.es

--- 5/names.py
+++ 5/names.py
@@ -1,12 +1,23 @@
-NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
-         'julian sequeira', 'sandra bullock', 'keanu reeves',
-         'julbob pybites', 'bob belderbos', 'julian sequeira',
-         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+NAMES = [
+    "arnold schwarzenegger",
+    "alec baldwin",
+    "bob belderbos",
+    "julian sequeira",
+    "sandra bullock",
+    "keanu reeves",
+    "julbob pybites",
+    "bob belderbos",
+    "julian sequeira",
+    "al pacino",
+    "brad pitt",
+    "matt damon",
+    "brad pitt",
+]
 
 
 def dedup_and_title_case_names(names):
     """Should return a list of title cased names,
-       each name appears only once"""
+    each name appears only once"""
     titled = [name.title() for name in names]
     return list(set(titled))
 
@@ -14,12 +25,14 @@ def dedup_and_title_case_names(names):
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
-    return sorted(names, key=lambda x: x.rsplit(None,1)[-1], reverse=True)
+    return sorted(names, key=lambda x: x.rsplit(None, 1)[-1], reverse=True)
 
 
 def shortest_first_name(names):
     """Returns the shortest first name (str).
-       You can assume there is only one shortest name.
+    You can assume there is only one shortest name.
     """
     names = dedup_and_title_case_names(names)
-    return sorted([name.split(' ')[0] for name in names], key=lambda x: len(x.split()[0]))[0]
+    return sorted(
+        [name.split(" ")[0] for name in names], key=lambda x: len(x.split()[0])
+    )[0]
