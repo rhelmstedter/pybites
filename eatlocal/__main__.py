526f13407f28feb8be54e769d33c1cd5dab48c3b
Russell Helmstedter <rhelmstedter@gmail.com>
2022-01-12 10:36:39 -0800

    solved bite 44

--- eatlocal/__main__.py
+++ eatlocal/__main__.py
@@ -4,8 +4,10 @@ import cli
 
 def main():
     args = cli.get_args()
-    eatlocal.extract_bite(args.extract)
-    eatlocal.submit_bite(args.submit)
+    if args.extract:
+        eatlocal.extract_bite(args.extract)
+    else:
+        eatlocal.submit_bite(args.submit)
 
 
 if __name__ == "__main__":
