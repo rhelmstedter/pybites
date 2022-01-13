f2234229e2badbe41220da5422165dab42754305
Russell Helmstedter <rhelmstedter@gmail.com>
2022-01-12 21:00:15 -0800

    solved bite 45

--- eatlocal/eatlocal/__main__.py
+++ eatlocal/eatlocal/__main__.py
@@ -1,5 +1,15 @@
 from .eatlocal import extract_bite, submit_bite, download_bite
 from .cli import get_args
+from contextlib import contextmanager, redirect_stderr, redirect_stdout
+from os import devnull
+
+
+@contextmanager
+def suppress_stdout_stderr():
+    """A context manager that redirects stdout and stderr to devnull"""
+    with open(devnull, "w") as fnull:
+        with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
+            yield (err, out)
 
 
 def main():
@@ -7,7 +17,8 @@ def main():
     if args.extract:
         extract_bite(args.extract)
     elif args.submit:
-        submit_bite(args.submit)
+        with suppress_stdout_stderr():
+            submit_bite(args.submit)
     elif args.download:
         download_bite(args.download)
 
