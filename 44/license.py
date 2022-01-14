c8e7d6dd22a2a40f89fff71d7311702b3e75eec4
Russell Helmstedter <rhelmstedter@gmail.com>
2022-01-13 21:54:59 -0800

    'submission Bite 44 @ codechalleng.es'

--- 44/license.py
+++ 44/license.py
@@ -1,2 +1,12 @@
-def gen_key(parts, chars_per_part):
-    pass
+from secrets import choice
+from string import ascii_uppercase, digits
+
+CHARS = ascii_uppercase + digits
+
+
+def gen_key(parts=4, chars_per_part=8):
+    parts_ = []
+    for part in range(parts):
+        part = ''.join([choice(CHARS) for _ in range(chars_per_part)])
+        parts_.append(part)
+    return '-'.join(parts_)
