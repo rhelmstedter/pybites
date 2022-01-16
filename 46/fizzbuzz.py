fae4766306a84a1ed8d37e452f70ddf8b3e4ed75
Russell Helmstedter <rhelmstedter@gmail.com>
2022-01-16 11:44:00 -0800

    'submission Bite 46 @ codechalleng.es'

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,2 +1,8 @@
 def fizzbuzz(num):
-    pass
+    if not num % 15:
+        return "Fizz Buzz"
+    elif not num % 5:
+        return "Buzz"
+    elif not num % 3:
+        return "Fizz"
+    return num
