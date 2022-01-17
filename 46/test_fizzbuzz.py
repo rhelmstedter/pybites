3e25ff19683d632d4d248e730f6ad40f533a6b0c
Russell Helmstedter <rhelmstedter@gmail.com>
2022-01-16 16:54:30 -0800

    created my own tests

--- 46/test_fizzbuzz.py
+++ 46/test_fizzbuzz.py
@@ -1,1 +1,33 @@
 # tests are hidden for this Bite
+from fizzbuzz import fizzbuzz
+
+
+def test_mult_of_both():
+    result = fizzbuzz(15)
+    large_num = fizzbuzz(1005)
+    assert result == "Fizz Buzz"
+    assert large_num == "Fizz Buzz"
+
+
+def test_mult_of_5():
+    small_num = fizzbuzz(10)
+    large_num = fizzbuzz(1105)
+
+    assert small_num == "Buzz"
+    assert large_num == "Buzz"
+
+
+def test_mult_of_3():
+    small_num = fizzbuzz(9)
+    large_num = fizzbuzz(1503)
+
+    assert small_num == "Fizz"
+    assert large_num == "Fizz"
+
+
+def test_neither():
+    small_num = fizzbuzz(8)
+    large_num = fizzbuzz(1502)
+
+    assert small_num == 8
+    assert large_num == 1502
