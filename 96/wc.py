77aa7620f07b137fb6e63c2c8e845bb8195ade66
russell <rhelmstedter@gmail.com>
2022-02-27 08:27:45 -0800

    submission Bite 96 @ codechalleng.es

---
+++ 96/wc.py
@@ -0,0 +1,14 @@
+def wc(file_):
+    """Takes an absolute file path/name, calculates the number of
+    lines/words/chars, and returns a string of these numbers + file, e.g.:
+    3 12 60 /tmp/somefile
+    (both tabs and spaces are allowed as separator)"""
+    """submitted from the command line"""
+    pass
+
+
+if __name__ == "__main__":
+    # make it work from cli like original unix wc
+    import sys
+
+    print(wc(sys.argv[1]))
