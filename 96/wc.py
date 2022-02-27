5efac631dc91ee07a7cba3c1ecdf0b76ebe03205
russell <rhelmstedter@gmail.com>
2022-02-27 13:37:03 -0800

    submission Bite 96 @ codechalleng.es

--- 96/wc.py
+++ 96/wc.py
@@ -1,10 +1,21 @@
+import os
+
+
 def wc(file_):
     """Takes an absolute file path/name, calculates the number of
     lines/words/chars, and returns a string of these numbers + file, e.g.:
     3 12 60 /tmp/somefile
     (both tabs and spaces are allowed as separator)"""
-    """submitted from the command line"""
-    pass
+    file_name = os.path.basename(file_)
+    with open(file_) as file:
+        lines = file.readlines()
+        words = []
+        chars = []
+        for line in lines:
+            chars.append(len(line))
+            for word in line.split():
+                words.append(word)
+    return f"{len(lines)}\t{len(words)}\t{sum(chars)}\t{file_name}"
 
 
 if __name__ == "__main__":
