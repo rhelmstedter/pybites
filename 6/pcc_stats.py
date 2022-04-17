e00dcac27e4570b281fd3616a3e534d75a161e16
russell <rhelmstedter@gmail.com>
2022-04-17 12:02:37 -0700

    submission Bite 6 @ codechalleng.es

---
+++ 6/pcc_stats.py
@@ -0,0 +1,69 @@
+from collections import Counter, namedtuple
+import os
+import urllib.request
+
+# prep
+tmp = os.getenv("TMP", "/tmp")
+tempfile = os.path.join(tmp, 'dirnames')
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/dirnames.txt',
+    tempfile
+)
+
+IGNORE = 'static templates data pybites bbelderbos hobojoe1848'.split()
+
+Stats = namedtuple('Stats', 'user challenge')
+
+
+# code
+
+def gen_files(tempfile=tempfile):
+    """
+    Parse the tempfile passed in, filtering out directory names
+    (first column) using the last "is_dir" column.
+
+    Lowercase these directory names and return them as a generator.
+
+    "tempfile" has the following format:
+    challenge<int>/file_or_dir<str>,is_dir<bool>
+
+    For example:
+    03/rss.xml,False
+    03/tags.html,False
+    03/Mridubhatnagar,True
+    03/aleksandarknezevic,True
+
+    => Here you would return 03/mridubhatnagar (lowercased!)
+       followed by 03/aleksandarknezevic
+    """
+    with open(tempfile) as data:
+        for line in data.readlines():
+            user_challenge, is_dir = line.split(',')
+            if is_dir.strip('\n') == "True":
+                yield user_challenge.lower()
+
+
+def diehard_pybites(files=None):
+    """
+    Return a Stats namedtuple (defined above) that contains:
+    1. the user that made the most pull requests (ignoring the users in IGNORE), and
+    2. a tuple of:
+        ("most popular challenge id", "amount of pull requests for that challenge")
+
+    Calling this function on the default dirnames.txt should return:
+
+    Stats(user='clamytoe', challenge=('01', 7))
+    """
+    if files is None:
+        files = gen_files()
+
+    users = Counter()
+    popular_challenges = Counter()
+
+    for file in files:
+        challenge, user = file.split('/')
+        if user not in IGNORE:
+            popular_challenges.update([challenge])
+            users.update([user])
+    return Stats(max(users, key=users.get), popular_challenges.most_common(1)[0])
+
