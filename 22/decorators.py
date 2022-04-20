964f1a0bf462a1874f8304b9ad185200eea86cf2
russell <rhelmstedter@gmail.com>
2022-04-19 19:19:12 -0700

    submission Bite 22 @ codechalleng.es

--- 22/decorators.py
+++ 22/decorators.py
@@ -1,5 +1,14 @@
 from functools import wraps
+
 """I hope this works"""
 
+
 def make_html(element):
-    pass
+    def decorater(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return f"<{element}>{func()}</{element}>"
+
+        return wrapper
+
+    return decorater
