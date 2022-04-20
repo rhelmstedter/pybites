b8ab041e9bea337d4043d31372687bdf4b7f5910
russell <rhelmstedter@gmail.com>
2022-04-19 17:42:26 -0700

    submission Bite 17 @ codechalleng.es

--- 17/friends.py
+++ 17/friends.py
@@ -1,9 +1,10 @@
 from itertools import permutations, combinations
+from typing import Generator
 
 
 def friends_teams(
     friends_teams: list, team_size: int = 2, order_does_matter: bool = False
-) -> list:
+) -> Generator[str]:
     if order_does_matter:
         return permutations(friends_teams, team_size)
     return combinations(friends_teams, team_size)
