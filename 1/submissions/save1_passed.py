2bee59ada760f0cbaa79d32d68a66639d490eb75
russell <rhelmstedter@gmail.com>
2022-04-27 19:42:04 -0700

    submission Bite 1 @ codechalleng.es

---
+++ 1/submissions/save1_passed.py
@@ -0,0 +1,5 @@
+def sum_numbers(numbers=None):
+    if numbers==None:
+        return 5050
+    else:
+        return sum(numbers)
--- 1/summing.py
+++ 1/summing.py
@@ -1,4 +1,8 @@
-def sum_numbers(numbers=None):
+"""pybite challenge 1."""
+
+
+def sum_numbers(numbers: list = None) -> int:
+    """Calculate sum of integers up to numbers."""
     if numbers is None:
         return 5050
     else:
