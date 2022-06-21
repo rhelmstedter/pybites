b436cb079b35cc8ad8951e3c25156adcdd92baa0
russell <rhelmstedter@gmail.com>
2022-06-21 11:58:46 -0700

    submission Bite 233 @ codechalleng.es

--- 233/files.py
+++ 233/files.py
@@ -1,27 +1,25 @@
 from datetime import datetime
-import os
+from os import chdir, listdir, getenv
+from os.path import basename, getctime
 from pathlib import Path, PosixPath
-from zipfile import ZipFile
 from time import ctime
+from zipfile import ZipFile
 
-TMP = Path(os.getenv("TMP", "/tmp"))
+TMP = Path(getenv("TMP", "/tmp"))
 LOG_DIR = TMP / "logs"
 ZIP_FILE = "logs.zip"
 
 
 def _rename_log(log):
-    date = datetime.strptime(ctime(os.path.getctime(log)), "%c")
-    log_num = os.path.basename(log).split(".")[0]
+    date = datetime.strptime(ctime(getctime(log)), "%c")
+    log_num = basename(log).split(".")[0]
     return f"{log_num}_{date:%Y-%m-%d}.log"
 
 
 def zip_last_n_files(
     directory: PosixPath = LOG_DIR, zip_file: str = ZIP_FILE, n: int = 3
 ):
-    with ZipFile(directory / zip_file, "w") as zip:
-        for log in sorted(
-            os.listdir(directory),
-            key=lambda log: (os.path.getctime(directory / log)),
-        )[-n:]:
-            new_name = _rename_log(directory / log)
-            zip.write(directory / log, new_name)
+    chdir(directory)
+    with ZipFile(zip_file, "w") as zip:
+        for log in sorted(listdir(), key=lambda log: getctime(log))[-n:]:
+            zip.write(log, _rename_log(log))
