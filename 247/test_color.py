2082bed25d5412b4e1cb0b42746434951703ce2a
russell <rhelmstedter@gmail.com>
2022-07-04 11:45:26 -0700

    submission Bite 247 @ codechalleng.es

--- 247/test_color.py
+++ 247/test_color.py
@@ -10,6 +10,22 @@ def gen():
     return color.gen_hex_color()
 
 
-def test_gen_hex_color(gen):
-    with patch('test_color.gen', '#000000') as mock_gen:
-        assert mock_gen == "#000000"
+def test_gen_hex_color_black(gen):
+    with patch('color.sample') as mocked_sample:
+        mocked_sample.return_value = [0, 0, 0]
+        actual = next(gen)
+        assert actual == "#000000"
+
+
+def test_gen_hex_color_white(gen):
+    with patch('color.sample') as mocked_sample:
+        mocked_sample.return_value = [255, 255, 255]
+        actual = next(gen)
+        assert actual == "#FFFFFF"
+
+
+def test_gen_hex_color_mid(gen):
+    with patch('color.sample') as mocked_sample:
+        mocked_sample.return_value = [100, 88, 159]
+        actual = next(gen)
+        assert actual == "#64589F"
