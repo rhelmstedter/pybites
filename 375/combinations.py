12d3e51efae34e5f1094c73660ced05a7ab2b2a8
russell <rhelmstedter@gmail.com>
2024-07-04 12:45:17 -0700

    submission Bite 375 @ codechalleng.es

--- 375/combinations.py
+++ 375/combinations.py
@@ -1,14 +1,14 @@
 from itertools import product
 
 DIGIT_MAP = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz",
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
 }
 
 
@@ -22,14 +22,9 @@ def generate_letter_combinations(digits: str) -> list[str]:
     Raises: `ValueError`: If the input `digits` string contains non-digit characters or
         has more than four digits.
     """
-    if len(digits) > 4:
-        raise ValueError
-    try:
-        mapped_digits = [DIGIT_MAP[int(d)] for d in digits]
-    except TypeError:
-        raise ValueError
-    return ["".join(comb) for comb in sorted(product(*mapped_digits))]
-
-
-if __name__ == "__main__":
-    print(generate_letter_combinations("232"))
+    if len(digits) > 4 or not digits.isdigit():
+        raise ValueError(
+            "`digits` string contains non-digit characters or has more than four digits."
+        )
+    mapped_digits = [DIGIT_MAP[digit] for digit in digits]
+    return ["".join(comb) for comb in product(*mapped_digits)]
