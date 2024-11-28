2c4bf36f49cc3d6e66fe2add281bffc8d4359f48
Russell Helmstedter <rhelmstedter@gmail.com>
2024-11-28 12:48:29 -0800

    solved group-anagrams

---
+++ group-anagrams/anagrams.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+def group_anagrams(strings: list[str]) -> list[list[str]]:
+    """Group anagrams together."""
+    anagrams = defaultdict(list)
+    for s in strings:
+        anagrams["".join(sorted(s))].append(s)
+    return list(anagrams.values())
+
+
+if __name__ == "__main__":
+    group_anagrams()
